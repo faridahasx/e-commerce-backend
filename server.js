4933cdd0990a7e3b7fd4438f4540249ed463da74
Farida Hasanova <finance.labs1@gmail.com>
2023-04-03 19:41:31 +0400

    origins

--- server.js
+++ server.js
@@ -52,8 +52,7 @@ app.use(passpost.initialize());
 // CORS options
 app.use(cors({
     origin:[
-        'https://sw-shop.netlify.app',
-        'http://127.0.0.1:5173/'
+        '*'
     ],
     methods: "GET, POST, PUT, DELETE, PATCH",
     credentials:true
