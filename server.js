fa893a12dcf07b42d067f300d80011f6f17f0ab4
Farida Hasanova <finance.labs1@gmail.com>
2023-04-03 20:15:22 +0400

    origins

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(passpost.initialize());
 // CORS options
 app.use(cors({
     origin:[
-        '*'
+        'http://127.0.0.1:5173/'
     ],
     methods: "GET, POST, PUT, DELETE, PATCH",
     credentials:true
